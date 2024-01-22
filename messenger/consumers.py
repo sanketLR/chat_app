2d7eb5dadbac1b40433906afcc874463aae8b16b
sanketLR <sanket.c@logicrays.com>
2024-01-22 19:24:55 +0530

    message encrypt decrypt

--- messenger/consumers.py
+++ messenger/consumers.py
@@ -5,6 +5,14 @@ from channels.exceptions import StopConsumer
 from messenger.models import Message,Chat_Room
 from django.contrib.auth.models import User
 import datetime
+import base64
+import os
+
+#cryptography
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 class SimpleChatConsumer(AsyncWebsocketConsumer):
@@ -53,7 +61,12 @@ class SimpleChatConsumer(AsyncWebsocketConsumer):
         formatted_time = now_time.strftime("%d-%m-%Y %H:%M:%S")
 
         if message:
-            await self.save_chat_message(message, username, formatted_time, room)
+            
+            encrypted_message = self.encrypt_message(message)
+
+            await self.save_chat_message(encrypted_message, username, formatted_time, room)
+
+        client_msg["msg"] = encrypted_message
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -73,30 +86,34 @@ class SimpleChatConsumer(AsyncWebsocketConsumer):
         '''
 
         client_msg_time = event["now_time"]
+        client_msg = event["message"]
 
-        client_msg = event["message"] 
+        decrypted_message = self.decrypt_message(client_msg["msg"])
 
-        message = client_msg["msg"]
+        message = decrypted_message if decrypted_message else "Decryption failed"
+
+        decodedMessage = message
+
         username = client_msg["user"]
         room = client_msg["room"]
-
         time = client_msg_time
 
-        if message !=  "":
-            
-            await self.send(text_data=json.dumps({
-                "message" : message,
-                "username" : username,
-                "now_time" : time,
-                "room" : room
-            }))
+        await self.send(text_data=json.dumps({
+            "message": decodedMessage,
+            "username": username,
+            "now_time": time,
+            "room": room
+        }))
+
 
     @sync_to_async
     def save_chat_message(self, message, username, now_time, room):
+
         get_user = User.objects.filter(username = username).first()
         get_room = Chat_Room.objects.filter(cr_name = room).first()
 
         if get_user is not None:
+
             Message.objects.create(
                 room = get_room,
                 user = get_user,
@@ -104,11 +121,65 @@ class SimpleChatConsumer(AsyncWebsocketConsumer):
                 date_added = now_time
             )
 
+            
+    def derive_key(self, password):
+
+        salt = b'salt_salt_salt'
+
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), salt=salt, iterations=100000, length=32, backend=default_backend())
+
+        key = base64.urlsafe_b64encode(kdf.derive(password.encode()))[:32] 
+
+        return key
+
+
+    def encrypt_message(self, message):
+
+        password = "secret_password"
+
+        key = self.derive_key(password)
+
+        iv = os.urandom(16)
+
+        cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+
+        encryptor = cipher.encryptor()
+
+        ciphertext = encryptor.update(message.encode()) + encryptor.finalize()
+
+        encrypted_message = iv + ciphertext
+
+        return base64.urlsafe_b64encode(encrypted_message).decode()
+    
+
+    def decrypt_message(self, encrypted_message):
+
+        password = "secret_password"
+
+        key = self.derive_key(password)
+
+        data = base64.urlsafe_b64decode(encrypted_message)
+
+        iv = data[:16]
+
+        ciphertext = data[16:]
+
+        cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+
+        decryptor = cipher.decryptor()
+
+        decrypted_message = decryptor.update(ciphertext) + decryptor.finalize()
+
+        return decrypted_message.decode("utf-8")
+    
+
     async def disconnect(self,event):
+
         self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        
         raise StopConsumer()
 
 
--- messenger/serializers.py
+++ messenger/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from . models import *
+from messenger.models import (
+    Message,
+    Chat_Room
+)
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import base64
+
 
 class UserCreateSerializer(serializers.ModelSerializer):
 
@@ -9,10 +18,10 @@ class UserCreateSerializer(serializers.ModelSerializer):
         fields=['id','username', 'password']
       
 
-
 class MessageSerializer(serializers.ModelSerializer):
-    # Define a SerializerMethodField for the 'user' field
+
     user = serializers.SerializerMethodField()
+    content = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
@@ -23,7 +32,36 @@ class MessageSerializer(serializers.ModelSerializer):
             "date_added",
         ]
 
-    # Define the method to get the 'username' for the 'user' field
+    def derive_key(self, password):
+
+        salt = b'salt_salt_salt'
+
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), salt=salt, iterations=100000, length=32, backend=default_backend())
+
+        key = base64.urlsafe_b64encode(kdf.derive(password.encode()))[:32]  # Truncate to 32 bytes (256 bits)
+
+        return key
+    
+    def get_content(self, obj):
+            
+        password = "secret_password"
+
+        key = self.derive_key(password)
+
+        data = base64.urlsafe_b64decode(obj.content)
+        
+        iv = data[:16]
+
+        ciphertext = data[16:]
+
+        cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+        
+        decryptor = cipher.decryptor()
+
+        decrypted_message = decryptor.update(ciphertext) + decryptor.finalize()
+
+        return decrypted_message.decode("utf-8")
+
     def get_user(self, obj):
         return obj.user.username
 
